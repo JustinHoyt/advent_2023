beee7850b22d33fef4caa50fcbef0eaaab5f0b7a
justinhoyt <justinhoyt@gmail.com>
2023-12-21 21:05:16 +0000

    Refactor day 1 and 2 to use Deno tests

--- day_1_trebuchet.ts
+++ day_1_trebuchet.ts
@@ -1,32 +1,62 @@
-const lines = Deno.readTextFileSync("./day_1_trebuchet_input.txt").split("\n");
+import * as mod from "https://deno.land/std@0.209.0/assert/mod.ts";
 
-// Part 1
-console.log(lines.reduce((sum: number, line: string) => {
-  const digits = line.match(/\d/g);
-  if (digits == null || digits.length === 0) return sum;
+function trebuchet1(lines: string[]): number {
+  return lines.reduce((sum: number, line: string) => {
+    const digits = line.match(/\d/g);
+    if (digits == null || digits.length === 0) return sum;
 
-  return sum + Number(`${digits.at(0)}${digits.at(-1)}`);
-}, 0));
+    return sum + Number(`${digits.at(0)}${digits.at(-1)}`);
+  }, 0);
+}
 
-// Part 2
-console.log(lines.reduce((sum: number, line: string) => {
-  const newLine = [
-    { name: "one", replacement: "o1e" },
-    { name: "two", replacement: "t2o" },
-    { name: "three", replacement: "t3e" },
-    { name: "four", replacement: "f4r" },
-    { name: "five", replacement: "f5e" },
-    { name: "six", replacement: "s6x" },
-    { name: "seven", replacement: "s7n" },
-    { name: "eight", replacement: "e8t" },
-    { name: "nine", replacement: "n9e" },
-  ].reduce(
-    (tempLine, { name, replacement }) => tempLine.replaceAll(name, replacement),
-    line,
+function trebuchet2(lines: string[]): number {
+  return lines.reduce((sum: number, line: string) => {
+    const newLine = [
+      { name: "one", replacement: "o1e" },
+      { name: "two", replacement: "t2o" },
+      { name: "three", replacement: "t3e" },
+      { name: "four", replacement: "f4r" },
+      { name: "five", replacement: "f5e" },
+      { name: "six", replacement: "s6x" },
+      { name: "seven", replacement: "s7n" },
+      { name: "eight", replacement: "e8t" },
+      { name: "nine", replacement: "n9e" },
+    ].reduce(
+      (tempLine, { name, replacement }) => tempLine.replaceAll(name, replacement),
+      line,
+    );
+
+    const digits = newLine.match(/\d/g);
+    if (digits == null || digits.length === 0) return sum;
+
+    return sum + Number(`${digits.at(0)}${digits.at(-1)}`);
+  }, 0);
+}
+
+if(import.meta.main) {
+  console.log(trebuchet1(
+    Deno.readTextFileSync("./day_1_trebuchet_input.txt").split("\n"),
+  ));
+  console.log(trebuchet2(
+    Deno.readTextFileSync("./day_1_trebuchet_input.txt").split("\n"),
+  ));
+}
+
+Deno.test(function realCase1() {
+  mod.assertEquals(
+    trebuchet1(
+      Deno.readTextFileSync("./day_1_trebuchet_input.txt").split("\n"),
+    ),
+    54877,
   );
+});
 
-  const digits = newLine.match(/\d/g);
-  if (digits == null || digits.length === 0) return sum;
+Deno.test(function realCase2() {
+  mod.assertEquals(
+  trebuchet2(
+      Deno.readTextFileSync("./day_1_trebuchet_input.txt").split("\n"),
+    ),
+    54100,
+  );
+});
 
-  return sum + Number(`${digits.at(0)}${digits.at(-1)}`);
-}, 0));
--- day_2_cube_conundrum.ts
+++ day_2_cube_conundrum.ts
@@ -1,49 +1,73 @@
-const lines = Deno.readTextFileSync("./day_2_cube_conundrum_input.txt").split(
-  "\n",
-);
-lines.pop();
+import * as mod from "https://deno.land/std@0.209.0/assert/mod.ts";
 
 const MAX_RED_ALLOWED = 12;
 const MAX_GREEN_ALLOWED = 13;
 const MAX_BLUE_ALLOWED = 14;
 
-const part1 = lines.reduce((sum: number, line: string, index: number) => {
-  const getMatchedNumber = ([match, ..._rest]: RegExpMatchArray) =>
-    Number(match);
+function cubeConundrum1(lines: string[]) {
+  return lines.reduce((sum: number, line: string, index: number) => {
+    const getMatchedNumber = ([match, ..._rest]: RegExpMatchArray) =>
+      Number(match);
 
-  const maxCubesSeen = (color: string): number => {
-    const numbersAssociatedWithColor = new RegExp(`\\d+(?= ${color})`, "g");
-    return [...line.matchAll(numbersAssociatedWithColor)]
-      .map(getMatchedNumber)
-      .reduce((a, b) => Math.max(a, b));
-  };
+    const maxCubesSeen = (color: string): number => {
+      const numbersAssociatedWithColor = new RegExp(`\\d+(?= ${color})`, "g");
+      return [...line.matchAll(numbersAssociatedWithColor)]
+        .map(getMatchedNumber)
+        .reduce((a, b) => Math.max(a, b), -Infinity);
+    };
 
-  return ([
-      ["red", MAX_RED_ALLOWED],
-      ["green", MAX_GREEN_ALLOWED],
-      ["blue", MAX_BLUE_ALLOWED],
-    ] as const).every(([color, maxAllowed]) =>
-      maxCubesSeen(color) <= maxAllowed
-    )
-    ? sum + index + 1
-    : sum;
-}, 0);
+    return ([
+        ["red", MAX_RED_ALLOWED],
+        ["green", MAX_GREEN_ALLOWED],
+        ["blue", MAX_BLUE_ALLOWED],
+      ] as const).every(([color, maxAllowed]) =>
+        maxCubesSeen(color) <= maxAllowed
+      )
+      ? sum + index + 1
+      : sum;
+  }, 0);
+}
 
-console.log(part1);
+function cubeConundrum2(lines: string[]) {
+  return lines.reduce((sum: number, line: string) => {
+    const getMatchedNumber = ([match, ..._rest]: RegExpMatchArray) =>
+      Number(match);
 
-const part2 = lines.reduce((sum: number, line: string) => {
-  const getMatchedNumber = ([match, ..._rest]: RegExpMatchArray) =>
-    Number(match);
+    const maxCubesSeen = (color: string): number => {
+      const numbersAssociatedWithColor = new RegExp(`\\d+(?= ${color})`, "g");
+      return [...line.matchAll(numbersAssociatedWithColor)]
+        .map(getMatchedNumber)
+        .reduce((a, b) => Math.max(a, b), 0);
+    };
 
-  const maxCubesSeen = (color: string): number => {
-    const numbersAssociatedWithColor = new RegExp(`\\d+(?= ${color})`, "g");
-    return [...line.matchAll(numbersAssociatedWithColor)]
-      .map(getMatchedNumber)
-      .reduce((a, b) => Math.max(a, b));
-  };
+    return sum +
+      maxCubesSeen("red") * maxCubesSeen("green") * maxCubesSeen("blue");
+  }, 0);
+}
 
-  return sum +
-    maxCubesSeen("red") * maxCubesSeen("green") * maxCubesSeen("blue");
-}, 0);
+if(import.meta.main) {
+  console.log(cubeConundrum1(
+    Deno.readTextFileSync("./day_2_cube_conundrum_input.txt").split("\n"),
+  ));
+  console.log(cubeConundrum2(
+    Deno.readTextFileSync("./day_2_cube_conundrum_input.txt").split("\n"),
+  ));
+}
 
-console.log(part2);
+Deno.test(function realCase1() {
+  mod.assertEquals(
+    cubeConundrum1(
+      Deno.readTextFileSync("./day_2_cube_conundrum_input.txt").split("\n"),
+    ),
+    2694,
+  );
+});
+
+Deno.test(function realCase2() {
+  mod.assertEquals(
+  cubeConundrum2(
+      Deno.readTextFileSync("./day_2_cube_conundrum_input.txt").split("\n"),
+    ),
+    54699,
+  );
+});
